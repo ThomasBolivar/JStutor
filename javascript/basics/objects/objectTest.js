f7778128b0df21407850e92a1a6b8658f4f5722c
Tbolivar <thomassvpbolivar@gmail.com>
2019-08-11 15:46:06 +0400

    fetching,promises practice

---
+++ javascript/basics/objects/objectTest.js
@@ -0,0 +1,56 @@
+const person = Object.create({
+
+    
+
+},{
+    name:{
+        value: "Denis",
+        enumerable: true, // can be iterable through
+        writable:false, // can't rewrite age
+        configurable:false, // can't delete age
+    },
+    birthDate:{
+        value:1996,
+        enumerable:true,
+    },
+    age:{
+        get(){
+            return new Date().getFullYear()-this.birthDate;
+        },
+        set(value){
+            console.log('Set age', value);
+            
+        },
+    },
+    isAdmin:{
+        value:true,
+        enumerable:false,
+    }
+});
+person.name ="John";
+for(let key in person){
+    console.log('Key', key, person[key]);
+}
+
+// just for fun
+
+
+const sleep = ms => new Promise(resolve =>{
+    setTimeout(()=>resolve(),ms);
+
+});
+
+for(let i=1;i<10;i++){
+
+sleep(i+'000').then(()=>{
+    console.log('After ',i,' seconds')
+    document.body.style.background =colorRandomizer();
+});
+
+
+}
+
+function colorRandomizer(){
+    let hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+    return hue;
+}
---
+++ javascript/basics/promises/promisesTest.js
@@ -0,0 +1,73 @@
+
+
+/**
+ * 
+ * we can create chain of promises, to keep async away from inner code hell
+ * 
+ */
+
+
+console.log("Start");
+
+const promise = new Promise((resolve,reject)=>{
+
+        setTimeout(()=>{
+            console.log("preparing data...");
+
+            const backendData = {
+                name: "User Information",
+                port: 8080,
+                status: 200
+            }
+            resolve(backendData);
+           
+
+        },2000);
+
+
+
+});
+
+promise.then(data=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            data.isModified=true;
+            resolve(data);
+        },2000)
+    });
+    
+}).then(clientData=>{
+    console.log('Data recieved', clientData)
+    clientData.modifiedByPromise = true;
+    return clientData;
+    
+})
+.then(clientData=>{
+        console.log("Here's modified by promsie Data: ", clientData);
+
+}).catch(err=>console.error('Error',err))
+.finally(()=>{
+    console.log('Finally!');
+});
+
+
+const sleep = ms => new Promise(resolve =>{
+        setTimeout(()=>resolve(),ms);
+
+});
+
+for(let i=1;i<10;i++){
+sleep(i+'000').then(()=>console.log('After ',i,' seconds'));
+}
+
+// we can create array of promises
+
+//All Promises message will appear only then every other promise inside array finished
+Promise.all([sleep(11000),sleep(12000)]).then(()=>{
+    console.log('All Promises');
+})
+
+// Race Promises will appear only then at least one promise finished
+Promise.race([sleep(13000),sleep(14000)]).then(()=>{
+    console.log('Race Promises');
+})
